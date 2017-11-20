2b70714403c7b27b7a59ce55c59863fa66e240bb
Cameron Stark <starkc920@outlook.com>
2017-11-20 16:25:01 -0500

    added the website

---
+++ js/config/app_config.js
@@ -0,0 +1,11 @@
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/Home', {
+            templateUrl: 'views/Home.html'
+        })
+        
+        .otherwise({
+            redirectTo: '/Home'
+        })
+        
+}]);
---
+++ js/controllers/MainController.js
@@ -0,0 +1,77 @@
+app.controller('MainController', ['$scope', function($scope) {
+    
+    $scope.modules = [
+        {
+            name: 'Mt. Rainier Node 1',
+            lat: '46.8523',
+            lon: '-121.7603',
+            status: 'Working',
+            weight: '5 of 10lbs'
+        },
+           {
+            name: 'Mt. Olympus Node 7',
+            lat: '47.7170',
+            lon: '-123.3170',
+            status: 'Working',
+            weight: '5 of 10lbs'
+        },
+            {
+            name: 'Mt. Olympus Node 8',
+            lat: '47.8330',
+            lon: '-123.3170',
+            status: 'Not Working',
+            weight: '5 of 10lbs'
+        },
+            {
+            name: 'Mt. Olympus Node 9',
+            lat: '47.8830',
+            lon: '-123.6330',
+            status: 'Working',
+            weight: '5 of 10lbs'
+        },
+            {
+            name: 'Mt. Olympus Node 10',
+            lat: '47.4950',
+            lon: '13.6020',
+            status: 'Full',
+            weight: '10 of 10lbs'
+        },
+            {
+            name: 'North Cascades Node 11',
+            lat: '48.5500',
+            lon: '-120.9500',
+            status: 'Working',
+            weight: '5 of 10lbs'
+        },
+            {
+            name: 'Noth Cascades Node 12',
+            lat: '48.3170',
+            lon: '-120.9830',
+            status: 'Working',
+            weight: '5 of 10lbs'
+        },
+            {
+            name: 'North Cascades Node 13',
+            lat: '48.3670',
+            lon: '-121.0500',
+            status: 'Working',
+            weight: '5 of 10lbs'
+        },
+            {
+            name: 'Mt Olympus Node 13',
+            lat: '47.8170',
+            lon: '-123.6000',
+            status: 'Working',
+            weight: '5 of 10lbs'
+        },
+    
+
+        
+
+    ];
+    
+
+   
+
+
+}]);
